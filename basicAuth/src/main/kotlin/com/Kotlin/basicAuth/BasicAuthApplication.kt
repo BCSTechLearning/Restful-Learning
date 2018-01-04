ea37743a997d33ac04c6ffd1266b4f0ca9323c47
atul.kumar <vicky00065.ak@gmail.com>
2018-01-04 16:01:44 +0530

    adding basicAuth project files

--- basicAuth/src/main/kotlin/com/Kotlin/basicAuth/BasicAuthApplication.kt
+++ basicAuth/src/main/kotlin/com/Kotlin/basicAuth/BasicAuthApplication.kt
@@ -1,10 +1,14 @@
 package com.Kotlin.basicAuth
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.logging.Logger
 
 @SpringBootApplication
 class BasicAuthApplication
+
+
 
 fun main(args: Array<String>) {
     SpringApplication.run(BasicAuthApplication::class.java, *args)
--- basicAuth/src/main/kotlin/com/Kotlin/basicAuth/controllers/userControl.kt
+++ basicAuth/src/main/kotlin/com/Kotlin/basicAuth/controllers/userControl.kt
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @Api(value = "api", description = "Rest Api for the user login", tags = arrayOf("User API"))
 class userControl{
 
+    var Log = LoggerFactory.getLogger(this.javaClass.name)
+
     @GetMapping(value = arrayOf("/user/{name}"), produces = arrayOf("application/json"))
     @ApiOperation(value = "Display Welcome message to the Uer", response = data::class)
     @ApiResponses( value = arrayOf(
@@ -23,6 +26,7 @@ class userControl{
             ApiResponse(code = 404, message = "Resource not found")
     ))
     fun message(@PathVariable name: String): data {
+        Log.info("User Logged in")
         return data(name,  " Hello Admin " + name)
     }
 

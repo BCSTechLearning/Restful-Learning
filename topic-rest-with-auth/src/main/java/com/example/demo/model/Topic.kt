c92060fbf00cd3573a39f4f090dc387aa6fb8469
chriszer <chriszer.myth@gmail.com>
2018-01-15 18:51:17 +0800

    Rest_with_basic_auth

---
+++ topic-rest-with-auth/src/main/java/com/example/demo/model/Topic.kt
@@ -0,0 +1,29 @@
+package com.example.demo.model
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+class Topic {
+
+    @Id
+    var id: String? = null
+    var name: String? = null
+    var description: String? = null
+
+    /*public Topic() {
+
+	}*/
+
+    constructor(id: String, name: String, description: String) : super() {
+        this.id = id
+        this.name = name
+        this.description = description
+    }
+
+    constructor() {
+
+    }
+
+
+}

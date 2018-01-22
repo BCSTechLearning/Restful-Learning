6f40881c5e4a745cbce0e00276f09b5aade8bee0
chriszer <chriszer.myth@gmail.com>
2018-01-22 14:10:24 +0800

    delete oauth

--- course-api-data-oauth/src/main/java/com/example/demo/repository/TopicRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository
-
-import com.example.demo.model.Topic
-import org.springframework.data.repository.CrudRepository
-
-interface TopicRepository : CrudRepository<Topic, String>

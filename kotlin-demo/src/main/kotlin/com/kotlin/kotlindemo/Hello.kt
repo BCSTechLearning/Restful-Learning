147cf695d5073da412da076be418675776974e90
chriszer <chriszer.myth@gmail.com>
2018-03-04 23:58:24 +0800

    de

--- kotlin-demo/src/main/kotlin/com/kotlin/kotlindemo/Hello.kt
+++
@@ -1,5 +0,0 @@
-package com.kotlin.kotlindemo
-
-import org.springframework.stereotype.Component
-
-data class Hello(val message:String)
--- kotlin-demo/src/main/kotlin/com/kotlin/kotlindemo/HelloService.kt
+++
@@ -1,8 +0,0 @@
-package com.kotlin.kotlindemo
-
-import org.springframework.stereotype.Service
-
-@Service
-class HelloService {
-    fun getHello() = "Hello service!"
-}
